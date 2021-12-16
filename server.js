7d4a2c097cbb1086ac722a1f5fb055f4dfe7d4d1
nikhilgomase <nikhilgomase142@gmail.com>
2021-12-16 20:17:19 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-// nstall express server
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.use(express.static(__dirname + "/dist/space-xdashboard"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/space-xdashboard/index.html"));
+});
+app.listen(process.env.PORT || 8080);
