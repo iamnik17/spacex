35d028b4fbc17e47025bdda5d54890eb40644fb1
nikhilgomase <nikhilgomase142@gmail.com>
2021-12-16 20:26:34 +0530

    change app name in serve.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/space-xdashboard"));
+app.use(express.static(__dirname + "/dist/SpaceXDashboard"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/space-xdashboard/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/SpaceXDashboard/index.html"));
 });
 app.listen(process.env.PORT || 8080);
