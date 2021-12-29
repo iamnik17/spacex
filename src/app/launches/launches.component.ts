f25981553e918e9b9dd75c7811fe1d26f5f884ae
nikhilgomase <nikhilgomase142@gmail.com>
2021-12-29 17:22:05 +0530

    chaanges

--- src/app/launches/launches.component.ts
+++ src/app/launches/launches.component.ts
@@ -27,12 +27,30 @@ export class LaunchesComponent implements OnInit {
   // dataSource = ELEMENT_DATA;
   alwaysShowCalendars: boolean;
   ranges: any = {
-    'Today': [moment(), moment()],
-    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-    'This Month': [moment().startOf('month'), moment().endOf('month')],
-    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    'Past week': [
+      moment().subtract(1, 'year').startOf('year'),
+      moment().subtract(1, 'week').endOf('week'),
+    ],
+    'Past month': [
+      moment().subtract(1, 'month').startOf('month'),
+      moment().subtract(1, 'month').endOf('month'),
+    ],
+    'Past 3 months': [
+      moment().subtract(3, 'months').startOf('month'),
+      moment().subtract(1, 'month').endOf('month'),
+    ],
+    'Past 6 months': [
+      moment().subtract(6, 'month').startOf('month'),
+      moment().subtract(1, 'month').endOf('month'),
+    ],
+    'Past year': [
+      moment().subtract(1, 'year').startOf('year'),
+      moment().subtract(1, 'month').endOf('month'),
+    ],
+    'Past 2 years': [
+      moment().subtract(2, 'year').startOf('year'),
+      moment().subtract(1, 'month').endOf('month'),
+    ],
   }
   invalidDates: moment.Moment[] = [moment().add(2, 'days'), moment().add(3, 'days'), moment().add(5, 'days')];
 
